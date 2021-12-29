d433f4859623f5a9027b1b34df7f653144218628
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-12-29 20:49:08 +0900

    打刻完了時のメッセージを追加

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import (MessageEvent, PostbackEvent, TextMessage,
                             TextSendMessage)
 
-from templates import ATTEND_TEMPLATE, ATTEND_TIME_TEMPLATE, LOCATION_TEMPLATE
+from templates import (ATTEND_TEMPLATE, ATTEND_TIME_TEMPLATE,
+                       CHECKOUT_TEMPLATE, LOCATION_TEMPLATE)
 
 app = Flask(__name__)
 r = redis.from_url(os.environ.get("REDIS_URL"))
@@ -54,33 +55,36 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    buttons_template_message = ATTEND_TEMPLATE
+    message = ATTEND_TEMPLATE
     line_bot_api.reply_message(
         event.reply_token,
-        buttons_template_message)
+        message)
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
     data = event.postback.data
     user_id = event.source.user_id
+    key = f'{user_id}_attend'
     params = parse_qs(data)
     action = params['action'][0]
     if action == "attend":
         if params['manual'][0] == "false":
-            r.hset(f'{user_id}_attend', 'manual', 'true')
-            buttons_template_message = LOCATION_TEMPLATE
+            r.hset(key, 'manual', 'true')
+            message = LOCATION_TEMPLATE
         else:
-            buttons_template_message = TextSendMessage(
+            message = TextSendMessage(
                 text='出勤時刻を入力してください！',
                 quick_reply=ATTEND_TIME_TEMPLATE
             )
     elif action == "locate":
         location = params['location'][0]
-        r.hset(f'{user_id}_attend', 'location', location)
+        r.hset(key, 'location', location)
+        print(f"hash: {r.hgetall(key)}")
+        message = CHECKOUT_TEMPLATE
 
     line_bot_api.reply_message(
         event.reply_token,
-        buttons_template_message)
+        message)
 
 if __name__ == "__main__":
     app.run()
--- templates.py
+++ templates.py
@@ -2,65 +2,87 @@ from linebot.models import (DatetimePickerAction, PostbackAction, QuickReply,
                             QuickReplyButton, TextSendMessage)
 
 ATTEND_TEMPLATE = TextSendMessage(
-        text='勤務を開始します！選択してください！',
-        quick_reply=QuickReply(
-            items=[
-                QuickReplyButton(
-                    action=PostbackAction(
-                        label='出勤',
-                        display_text='出勤',
-                        data='action=attend&manual=false'
-                    )
-                ),
-                QuickReplyButton(
-                    action=PostbackAction(
-                        label='出勤（手入力）',
-                        display_text='出勤（手入力）',
-                        data='action=attend&manual=true'
-                    )
-                ),
-            ]
-        )
-    )
-
-LOCATION_TEMPLATE = TextSendMessage(
-        text='勤務場所を登録します！選択してください！',
-        quick_reply=QuickReply(
-            items=[
-                QuickReplyButton(
-                    action=PostbackAction(
-                        label='出社',
-                        display_text='出社',
-                        data='action=locate&location=office'
-                    )
-                ),
-                QuickReplyButton(
-                    action=PostbackAction(
-                        label='在宅勤務',
-                        display_text='在宅勤務',
-                        data='action=locate&location=fullremote'
-                    )
-                ),
-                QuickReplyButton(
-                    action=PostbackAction(
-                        label='在宅勤務4h未満',
-                        display_text='在宅勤務4h未満',
-                        data='action=locate&location=remotelt4h'
-                    )
-                )
-            ]
-        )
-    )
-
-ATTEND_TIME_TEMPLATE = QuickReply(
+    text='勤務を開始します！選択してください！',
+    quick_reply=QuickReply(
         items=[
             QuickReplyButton(
-                action=DatetimePickerAction(
-                    label='時刻を選択',
-                    data='action=time&type=attend',
-                    mode='time',
-                    initial='10:00'
+                action=PostbackAction(
+                    label='出勤',
+                    display_text='出勤',
+                    data='action=attend&manual=false'
+                )
+            ),
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='出勤（手入力）',
+                    display_text='出勤（手入力）',
+                    data='action=attend&manual=true'
+                )
+            ),
+        ]
+    )
+)
+
+LOCATION_TEMPLATE = TextSendMessage(
+    text='勤務場所を登録します！選択してください！',
+    quick_reply=QuickReply(
+        items=[
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='出社',
+                    display_text='出社',
+                    data='action=locate&location=office'
+                )
+            ),
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='在宅勤務',
+                    display_text='在宅勤務',
+                    data='action=locate&location=fullremote'
+                )
+            ),
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='在宅勤務4h未満',
+                    display_text='在宅勤務4h未満',
+                    data='action=locate&location=remotelt4h'
                 )
             )
         ]
     )
+)
+
+ATTEND_TIME_TEMPLATE = QuickReply(
+    items=[
+        QuickReplyButton(
+            action=DatetimePickerAction(
+                label='時刻を選択',
+                data='action=time&type=attend',
+                mode='time',
+                initial='10:00'
+            )
+        )
+    ]
+)
+
+CHECKOUT_TEMPLATE = TextSendMessage(
+    text='退勤します！選択してください！',
+    quick_reply=QuickReply(
+        items=[
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='退勤',
+                    display_text='退勤',
+                    data='action=checkout&manual=false'
+                )
+            ),
+            QuickReplyButton(
+                action=PostbackAction(
+                    label='退勤（手入力）',
+                    display_text='退勤（手入力）',
+                    data='action=checkout&manual=true'
+                )
+            ),
+        ]
+    )
+)
