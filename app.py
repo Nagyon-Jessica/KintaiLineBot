95bd2f39c3f5ef2654bf4ccb9d23f651f95fec0f
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-12-29 10:22:07 +0900

    送信元ユーザ識別を追加

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+ALLOWED_USERS = os.environ['ALLOWED_USER'].split(';')
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -29,6 +30,12 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     print("Request body: " + body)
+    print(f"params: {request.args}")
+
+    send_user = body['events'][0]['source']['userId']
+    if send_user not in ALLOWED_USERS:
+        print("Invalid access from the user {send_user}.")
+        abort(400)
 
     # handle webhook body
     try:
@@ -39,7 +46,6 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     buttons_template_message = TemplateSendMessage(
@@ -49,23 +55,21 @@ def handle_message(event):
             text='選択してください！',
             actions=[
                 PostbackAction(
-                    label='attend',
-                    display_text='出勤',
+                    label='出勤',
+                    display_text='attend',
                     data='action=attend&manual=false'
                 ),
                 PostbackAction(
-                    label='attend_by_hand',
-                    display_text='出勤（手入力）',
+                    label='出勤（手入力）',
+                    display_text='attend_by_hand',
                     data='action=buy&manual=true'
                 )
             ]
         )
     )
-
     line_bot_api.reply_message(
         event.reply_token,
         buttons_template_message)
-
 
 if __name__ == "__main__":
     app.run()
