435868d7d2906910a62281c0a46f0ff25fab8202
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-12-31 09:49:30 +0900

    出勤（手入力）の打刻処理追加

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 import os
+from datetime import date
 
 from linebot import LineBotApi
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 from templates import CHECKOUT_TEMPLATE
 
-SELENIUM_URL = os.environ.get("SELENIUM_URL")
+HOME_URL = os.environ.get("HOME_URL")
+AWTVIEW_URL = os.environ.get("AWTVIEW_URL")
 ID_YAGAO = os.environ.get("ID_YAGAO")
 PW_YAGAO = os.environ.get("PW_YAGAO")
 ID_KONB = os.environ.get("ID_KONB")
@@ -21,6 +24,9 @@ ALLOWED_USERS = os.environ['ALLOWED_USER'].split(';')
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 
+# 今日の日付を取得
+today = date.today()
+
 def attend(hash):
     """
     出勤処理
@@ -29,26 +35,25 @@ def attend(hash):
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     user_id = hash['user_id']
-    if "U9" in user_id:
-        login(driver, True)
-    else:
-        login(driver, False)
     if hash['manual'] == 'true':
-        register(driver)
+        driver.get(AWTVIEW_URL)
+        login(driver, user_id)
+        register(driver, True, hash['time'], hash['location'])
     else:
-        stamp(driver, True, location=hash['location'])
+        driver.get(HOME_URL)
+        login(driver, user_id)
+        stamp(driver, True, hash['location'])
     driver.quit()
     line_bot_api.push_message(
         to=user_id,
         messages=[CHECKOUT_TEMPLATE])
     return
 
-def login(driver, is_yagao):
+def login(driver, user_id):
     """
     Salesforceにログイン
     """
-    driver.get(SELENIUM_URL)
-    if is_yagao:
+    if "U9" in user_id:
         driver.find_element(by=By.ID, value='username').send_keys(ID_YAGAO)
         driver.find_element(by=By.ID, value='password').send_keys(PW_YAGAO)
     else:
@@ -84,8 +89,42 @@ def stamp(driver, is_attend, location=None):
         driver.find_element(by=By.ID, value='btnStInput').click()
     return
 
-def register(driver):
+def register(driver, is_attend, time, location=None):
     """
     「勤務表」画面上で時刻，備考を登録
+
+    :param bool is_attend: Trueなら出勤，Flaseなら退勤
+    :param str time: 出退勤時刻
+    :param str location: 1=出社, 2=在宅勤務, 3=在宅勤務4h未満
     """
+    # クリックできるように成るまで待機
+    wait = WebDriverWait(driver, 30)
+    wait.until(
+        EC.element_to_be_clickable((By.ID, f"ttvTimeSt{today}"))
+    )
+
+    # 該当日のtr要素を取得
+    today_row = driver.find_element(by=By.ID, value=f"dateRow{today}")
+    driver.find_element(by=By.ID, value=f"ttvTimeSt{today}").click()
+
+    # モーダルが表示されるまで待機
+    wait = WebDriverWait(driver, 30)
+    wait.until(
+        EC.presence_of_element_located((By.ID, "dialogInputTime"))
+    )
+
+    if is_attend:
+        input_id = 'startTime'
+    else:
+        input_id = 'endTime'
+
+    # 出退勤時刻を入力
+    driver.find_element(by=By.ID, value=input_id).send_keys(time)
+
+    # 勤務地を選択
+    select_element = driver.find_element(by=By.ID, value='workLocationId')
+    Select(select_element).select_by_index(int(location))
+
+    # 「登録」ボタン押下
+    driver.find_element(by=By.ID, value="dlgInpTimeOk").click()
     return
